e6ec7f163f8a201eb3b6dd4d1e15359feb287d60
Antonio gomez <aredcbtis16@hotmail.com>
2013-11-07 16:50:01 -0600

    subir4

---
+++ js/acciones.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('.btn').tap(function(){
+			var boton = (($(this).attr('class')).split(' '))[1];
+			switch(boton)
+			{
+			
+			case 'b1':
+			navigator.notification.beep(3);
+			break;
+			
+			case 'v1':
+			navigator.notification.vibrate(500);
+			break;
+			
+			
+			
+			}
+		});//tap
+	},false);
+});//document
